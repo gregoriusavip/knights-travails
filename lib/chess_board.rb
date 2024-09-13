584601e2a0c038c2ae422d2b6a7577a48e550f29
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-09-12 17:44:48 -0700

    Add tracing for shortest path

--- lib/chess_board.rb
+++ lib/chess_board.rb
@@ -14,7 +14,11 @@ class Board
 
   def knight_moves(pos, target)
     step = knight_moves_helper(pos, target)
+    p "You made it in #{step} move(s)! Here's your path:"
+    puts extract_path(target, pos) # trace from target back to position
   end
+
+  private
 
   def knight_moves_helper(pos, target)
     @board[pos[1]][pos[0]] = [pos[0], pos[1]]
@@ -47,5 +51,17 @@ class Board
       yield(x, y)
       queue << [x, y]
     end
+  end
+
+  def extract_path(pos, target)
+    return pos.to_s if pos.eql? target
+
+    res = pos
+    cur_pos = @board[pos[1]][pos[0]]
+    until cur_pos.eql? target
+      res = "#{cur_pos}\n#{res}"
+      cur_pos = @board[cur_pos[1]][cur_pos[0]]
+    end
+    "#{cur_pos}\n#{res}"
   end
 end
